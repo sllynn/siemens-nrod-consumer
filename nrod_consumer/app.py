a8e64a9e090fbd77ea76fa8e5523f9749321e7a8
sllynn <stuart@databricks.com>
2019-08-14 12:01:44 +0100

    added handler for mq

--- nrod_consumer/app.py
+++ nrod_consumer/app.py
@@ -1,7 +1,62 @@
 import json
+import stomp
+import boto3
+import socket
+import os
 
-# import requests
 
+kinesis_client = boto3.client('kinesis')
+MSGBROKERFQDN = "datafeeds.networkrail.co.uk"
+MSGBROKERPORT = 61618
+USERNAME = "andrew.weaver@databricks.com"
+PASSWORD = os.getenv("MSGBROKERPASSWORD")
+TOPIC = "/topic/TRAIN_MVT_HY_TOC"
+CLIENT_ID = socket.getfqdn()
+HEARTBEAT_INTERVAL_MS = 15000
+RECONNECT_DELAY_SECS = 15
+
+
+class StompClient(stomp.ConnectionListener):
+
+    def on_heartbeat(self):
+        print('Received a heartbeat')
+
+    def on_heartbeat_timeout(self):
+        print('ERROR: Heartbeat timeout')
+
+    def on_error(self, headers, message):
+        print('ERROR: %s' % message)
+
+    def on_disconnected(self):
+        print('Disconnected waiting %s seconds before exiting' % RECONNECT_DELAY_SECS)
+        time.sleep(RECONNECT_DELAY_SECS)
+        exit(-1)
+
+    def on_connecting(self, host_and_port):
+        print('Connecting to ' + host_and_port[0])
+
+    def on_message(self, headers, message):
+        try:
+            print('\n---\nGot message %s' % message)
+        except Exception as e:
+            print("\n\tError: %s\n--------\n" % str(e))
+
+
+def connect_and_subscribe(connection):
+    connection.start()
+
+    connect_header = {'client-id': USERNAME + '-' + CLIENT_ID}
+    subscribe_header = {'activemq.subscriptionName': CLIENT_ID}
+
+    connection.connect(username=USERNAME,
+                       passcode=PASSWORD,
+                       wait=True,
+                       headers=connect_header)
+
+    connection.subscribe(destination=TOPIC,
+                         id='1',
+                         ack='auto',
+                         headers=subscribe_header)
 
 def lambda_handler(event, context):
     """Sample pure Lambda function
@@ -25,13 +80,11 @@ def lambda_handler(event, context):
         Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
     """
 
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
+    conn = stomp.Connection12([(MSGBROKERFQDN, MSGBROKERPORT)],
+                              auto_decode=False,
+                              heartbeats=(HEARTBEAT_INTERVAL_MS, HEARTBEAT_INTERVAL_MS))
+    conn.set_listener('', StompClient())
+    connect_and_subscribe(conn)
 
     return {
         "statusCode": 200,
