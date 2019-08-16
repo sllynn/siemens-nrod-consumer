b0b3e4abc2b038c079c5bd6781c517f6088d1c64
sllynn <stuart@databricks.com>
2019-08-16 09:21:36 +0100

    added multiple lambda function defs for 4 TOCs

--- nrod_consumer/app.py
+++ nrod_consumer/app.py
@@ -1,27 +1,21 @@
 import json
 import stomp
 import boto3
+import base64
 import socket
 import os
 import time
 
+from botocore.exceptions import ClientError
 
-MSG_BROKER_HOST = "datafeeds.networkrail.co.uk"
-MSG_BROKER_PORT = 61618
-MSG_BROKER_USERNAME = os.getenv("MSG_BROKER_USERNAME")
-MSG_BROKER_PASSWORD = os.getenv("MSG_BROKER_PASSWORD")
-TOPIC = "/topic/TRAIN_MVT_HY_TOC"
 CLIENT_ID = socket.getfqdn()
 HEARTBEAT_INTERVAL_MS = 15000
 RECONNECT_DELAY_SECS = 15
 POLL_INTERVAL_SECS = 1
 POLL_ATTEMPTS = 90
-KINESIS_STREAM = "nrod-siemens"
 
 
 class StompClient(stomp.ConnectionListener):
-    def __init__(self):
-        self.kinesis_client = boto3.client("kinesis")
 
     def on_heartbeat(self):
         print('Received a heartbeat')
@@ -41,33 +35,87 @@ class StompClient(stomp.ConnectionListener):
         print('Connecting to {h}'.format(h=host_and_port[0]))
 
     def on_message(self, headers, message):
+        print("Received message")
         try:
-            print('\n---\nGot message %s' % message)
+            kinessis_put_records(message)
         except Exception as e:
             print("\n\tError: %s\n--------\n" % str(e))
-        msgs = json.loads(message)
-        self.kinesis_client.put_records(
-            Records=[dict(Data=json.dumps(msg), PartitionKey=msg["body"]["train_id"]) for msg in msgs],
-            StreamName="nrod-siemens")
 
 
-def connect_and_subscribe(connection):
+def kinessis_put_records(message):
+    msgs = json.loads(message)
+    kinesis_client = boto3.client("kinesis")
+    kinesis_client.put_records(
+        Records=[dict(Data=json.dumps(msg), PartitionKey=msg["body"]["train_id"]) for msg in msgs],
+        StreamName=os.getenv("KINESIS_STREAM"))
+
+
+def get_secret(secret_name):
+    region_name = "us-west-2"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    # In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+    # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    # We rethrow the exception by default.
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'DecryptionFailureException':
+            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+            # An error occurred on the server side.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            # You provided an invalid value for a parameter.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            # You provided a parameter value that is not valid for the current state of the resource.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            # We can't find the resource that you asked for.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+    else:
+        # Decrypts secret using the associated KMS CMK.
+        # Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if 'SecretString' in get_secret_value_response:
+            return get_secret_value_response['SecretString']
+        else:
+            return base64.b64decode(get_secret_value_response['SecretBinary'])
+
+
+def connect_and_subscribe(connection, credentials, topic):
     connection.start()
 
-    connect_header = {'client-id': MSG_BROKER_USERNAME + '-' + CLIENT_ID}
+    connect_header = {'client-id': credentials["MSG_BROKER_USERNAME"] + '-' + CLIENT_ID}
     subscribe_header = {
         'activemq.subscriptionName': CLIENT_ID,
         'activemq.prefetchSize': 1
     }
-    connection.connect(username=MSG_BROKER_USERNAME,
-                       passcode=MSG_BROKER_PASSWORD,
+    connection.connect(username=credentials["MSG_BROKER_USERNAME"],
+                       passcode=credentials["MSG_BROKER_PASSWORD"],
                        wait=True,
                        headers=connect_header)
 
-    connection.subscribe(destination=TOPIC,
+    connection.subscribe(destination=topic,
                          id='1',
                          ack='auto',
                          headers=subscribe_header)
+
 
 def lambda_handler(event, context):
     """Sample pure Lambda function
@@ -75,34 +123,21 @@ def lambda_handler(event, context):
     Parameters
     ----------
     event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
     context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
     Returns
     ------
     API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
     """
 
-    if not MSG_BROKER_PASSWORD:
-        return dict(
-            statusCode=401,
-            body=json.dumps(dict(message="no password supplied"))
-        )
-
-    conn = stomp.Connection12([(MSG_BROKER_HOST, MSG_BROKER_PORT)],
+    conn = stomp.Connection12([(os.getenv("MSG_BROKER_HOST"), os.getenv("MSG_BROKER_PORT"))],
                               auto_decode=False,
                               heartbeats=(HEARTBEAT_INTERVAL_MS, HEARTBEAT_INTERVAL_MS))
 
     conn.set_listener('', StompClient())
-    connect_and_subscribe(conn)
+    connect_and_subscribe(
+        connection=conn,
+        credentials=json.loads(get_secret("nrod-consumer-secrets")),
+        topic=os.getenv("MSG_BROKER_TOPIC"))
 
     for _ in range(POLL_ATTEMPTS):
         time.sleep(POLL_INTERVAL_SECS)
